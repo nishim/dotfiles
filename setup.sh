390277ec5b3715c43092e018d296d135d9df1e28
Yuji Nishimura <nishimura@sales-bank.com>
2023-07-14 09:23:30 +0900

    update

--- setup.sh
+++ setup.sh
@@ -6,3 +6,10 @@ ln -fs $PWD/.editorconfig ~/.editorconfig
 ln -fs $PWD/.gitconfig ~/.gitconfig
 ln -fs $PWD/.gitignore ~/.gitignore
 ln -fs $PWD/.vimrc ~/.vimrc
+
+
+mkdir -p ~/.zsh
+ln -fs $PWD/git-prompt.sh ~/.zsh/git-prompt.sh
+ln -fs $PWD/git-completion.bash ~/.zsh/git-completion.bash
+ln -fs $PWD/git-completion.zsh ~/.zsh/_git
+
